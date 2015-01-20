43074fda598f6642501e1e8caf9e6eceaa5c10ea
Krzysztof <sumiteru@wp.pl>
2015-01-20 16:49:33 +0100

    First commit

---
+++ KMPAlgorithm/KMP.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KMPAlgorithm
+{
+    class KMP
+    {
+        public static List<int> Search(string pattern, string text)
+        {
+            List<int> result = new List<int>();
+            int[] P = calculateArray(pattern);
+            int n = text.Length;
+            int m = pattern.Length;
+            int i = 0, j = 0;
+
+            while(i <= n - m)
+            {
+                j = P[j];
+                while ((j < m) && pattern[j].Equals(text[i + j]))
+                    j++;
+
+                if (j == m)
+                    result.Add(i);
+
+                i = i + Math.Max(1, j - P[j]);
+            }
+
+            return result;
+        }
+
+        public static int[] calculateArray(string pattern)
+        {
+            int t = 0;
+            int[] array = new int[pattern.Length + 1];
+            array[0] = 0;
+            array[1] = 0;
+
+            for (int i = 2; i <= pattern.Length; i++)
+            {
+                while ((t > 0) && (pattern[t] != pattern[i - 1]))
+                    t = array[t];
+
+                if (pattern[t].Equals(pattern[i - 1]))
+                    t++;
+
+                array[i] = t;
+            }
+
+            return array;
+        }
+    }
+}
---
+++ KMPAlgorithm/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KMPAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Wyniki w pliku wyjściowym.");
+                using (StreamWriter writer = new StreamWriter("file.out"))
+                {
+                    Console.SetOut(writer);
+                    string[] inputData = File.ReadAllLines("file.in");
+                    List<int> indexes = KMP.Search(inputData[0], inputData[1]);
+
+                    foreach (var i in indexes)
+                        Console.WriteLine("Element znaleziono na pozycji: {0}", i);
+
+                    int[] array = KMP.calculateArray(inputData[0]);
+                    foreach (var i in array)
+                        Console.Write("[{0}]", i);
+
+                    Console.WriteLine("\n");                
+                }
+            }
+            catch(FileNotFoundException)
+            {
+                Console.WriteLine("Nieprawidłowa ścieżka do pliku.");
+            }
+            finally
+            {
+                Console.SetOut(new StreamWriter(Console.OpenStandardOutput()));
+                Console.ReadKey();
+            }
+        }
+    }
+}
